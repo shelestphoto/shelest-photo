dbd6f4985452a628a5597bb712fe5079606469f4
Shelestovskii Ignat <shelestvskii.ignat@gmail.com>
2024-11-04 17:05:36 +0500

    тест

---
+++ src/app/example/page.tsx
@@ -0,0 +1,10 @@
+export default function ExamplePage (){
+    return (
+        <div className="container flex flex-col mx-auto">
+        <p className="text-lg">Здесь вы можете увидеть примеры работ</p>
+        <p className="text-lg">Мои работы</p>  
+    
+</div>
+       
+    )
+}
