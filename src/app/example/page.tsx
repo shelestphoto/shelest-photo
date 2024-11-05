b1d2bf1f23665470cccf49a61fd00a2a98fd4297
Shelestovskii Ignat <shelestvskii.ignat@gmail.com>
2024-11-05 20:27:52 +0500

    тест таблица

--- src/app/example/page.tsx
+++ src/app/example/page.tsx
@@ -1,10 +1,12 @@
 export default function ExamplePage (){
     return (
-        <div className="container flex flex-col mx-auto">
-        <p className="text-lg">Здесь вы можете увидеть примеры работ</p>
-        <p className="text-lg">Мои работы</p>  
-    
-</div>
-       
+<div className="container border border-white-200 flex flex-row grid grid-cols-6 m-auto">
+ <img className="col-span-2 md:h-full md:w-full" src="https://as2.ftcdn.net/v2/jpg/01/18/73/53/1000_F_118735384_xHRQIw8jkH08enDue9joHsgnN8W4WazK.jpg"/> 
+  <img className="col-span-2 md:h-full md:w-full" src="https://as2.ftcdn.net/v2/jpg/04/76/87/91/1000_F_476879175_0ErkgrAqqQdSvOsrnobBaDDfF7VuV7C3.jpg"/>
+   <img className="col-span-2 md:h-full md:w-full" src="https://as2.ftcdn.net/v2/jpg/02/85/87/17/1000_F_285871716_ypPApdIQbRVop1DlaM5NvvhZuqMf3Hpr.jpg"/> 
+    <img className="col-span-2 md:h-full md:w-full" src="https://as2.ftcdn.net/v2/jpg/05/96/77/41/1000_F_596774173_teOSqHb3P27p5tILGc5ZAlxj92QgJMzP.jpg"/>
+     <img className="col-span-2 md:h-full md:w-full" src="https://as1.ftcdn.net/v2/jpg/01/18/73/53/1000_F_118735384_xHRQIw8jkH08enDue9joHsgnN8W4WazK.jpg"/>
+      <img className="col-span-2 md:h-full md:w-full" src="https://as1.ftcdn.net/v2/jpg/09/99/30/58/1000_F_999305873_vu8dIHIQV57jNOv1rCLYFtEAFZTdzZVk.jpg"/>
+            </div>
     )
 }
