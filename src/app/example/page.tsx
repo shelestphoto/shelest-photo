44ed58e7fed63d2d7a7ffb909df8f1ba3762f42d
Shelestovskii Ignat <shelestvskii.ignat@gmail.com>
2024-11-19 19:41:04 +0500

    загрузка фотографий

--- src/app/example/page.tsx
+++ src/app/example/page.tsx
@@ -1,6 +1,21 @@
+"use client";
+import {UploadButton} from "~/utils/uploadthing";
+
 export default function ExamplePage (){
     return (
 <div className="container border border-white-200 flex flex-row grid grid-cols-6 m-auto">
+<UploadButton
+        endpoint="imageUploader"
+        onClientUploadComplete={(res) => {
+          // Do something with the response
+          console.log("Files: ", res);
+          alert("Upload Completed");
+        }}
+        onUploadError={(error: Error) => {
+          // Do something with the error.
+          alert(`ERROR! ${error.message}`);
+        }}
+      />
  <img className="col-span-2 md:h-full md:w-full" src="https://as2.ftcdn.net/v2/jpg/01/18/73/53/1000_F_118735384_xHRQIw8jkH08enDue9joHsgnN8W4WazK.jpg"/> 
   <img className="col-span-2 md:h-full md:w-full" src="https://as2.ftcdn.net/v2/jpg/04/76/87/91/1000_F_476879175_0ErkgrAqqQdSvOsrnobBaDDfF7VuV7C3.jpg"/>
    <img className="col-span-2 md:h-full md:w-full" src="https://as2.ftcdn.net/v2/jpg/02/85/87/17/1000_F_285871716_ypPApdIQbRVop1DlaM5NvvhZuqMf3Hpr.jpg"/> 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,8 @@
+import { withUt } from "uploadthing/tw";
 import { type Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
 
-export default {
+export default withUt({
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
@@ -11,4 +12,4 @@ export default {
     },
   },
   plugins: [require('@tailwindcss/forms'),],
-} satisfies Config;
+} ) satisfies Config;
